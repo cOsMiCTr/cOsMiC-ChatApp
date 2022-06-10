e6537ff20b7d0b89965e776f80f74177751201f2
cOsMiCTr <onurbaki@me.com>
2022-06-10 10:28:26 +0200

    Done

--- App.js
+++ App.js
@@ -1,39 +1,33 @@
 import React, { Component } from "react";
 // import the screens we want to navigate
-import Chat from './components/Chat';
-import Start from './components/Start';
+import Chat from "./components/Chat";
+import Start from "./components/Start";
 
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+//import react gesture handler
+import "react-native-gesture-handler";
+
+//import the navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+//Bottom navigator
+//import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+
 
 export default class HelloWorld extends Component {
-
-
   /* isPrimeNumber(number) takes a number and returns true if its a prime number */
 
   render() {
-    
     return (
       <NavigationContainer>
-               
-      <Stack.Navigator
-        initialRouteName="Start"
-      >
-        <Stack.Screen
-          name="Start"
-          component={Start}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
-      </Stack.Navigator>
-
-    </NavigationContainer>
+        {/* Set the initial page to start */}
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
+
--- components/Chat.js
+++ components/Chat.js
@@ -1,19 +1,120 @@
 import React from "react";
-import { View } from "react-native";
+//import the gifted chat
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { View, Platform, KeyboardAvoidingView } from "react-native";
 
 // The application’s main Chat component that renders the chat UI export default
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: "Your chat has been encrypted",
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  //Send Message
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: "#4F94CD", borderColor: "black", borderWidth: 0.15,     shadowColor: "#000000",
+            shadowOpacity: 0.8,
+            shadowRadius: 2,
+            shadowOffset: {
+              height: 1,
+              width: 1
+            }
+          },
+          right: {
+            backgroundColor: "#90EE90", borderColor: "black", borderWidth: 0.15,     shadowColor: "#000000",
+            shadowOpacity: 0.8,
+            shadowRadius: 2,
+            shadowOffset: {
+              height: 1,
+              width: 1
+            }
+          },
+        }}
+      />
+    );
+  }
+
   render() {
+    //get name
     let name = this.props.route.params.name;
+    //get selected color
     let { bgColor } = this.props.route.params;
-    this.props.navigation.setOptions({ title: "Hello " + name });
+    //Set name to navbar
+    this.props.navigation.setOptions({ title: name });
+
     return (
       <View
         style={{
           backgroundColor: bgColor,
           flex: 1,
         }}
-      ></View>
+      >
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
+      </View>
     );
   }
 }
+
+
+
+const styles = StyleSheet.create({
+  chatBubbleLeft: {
+    flex: 1,
+  },
+  chatBubbleRight: {
+    backgroundColor: "#90EE90", borderColor: "black", borderWidth: 0.15,     shadowColor: "#000000",
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 1
+    }
+  },
+});
+
